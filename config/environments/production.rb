4cf5b2a22d417bf2e71810ddb0808fe32907e994
Matt Snyder <snyder2112@me.com>
2012-12-04 13:13:26 -0500

    Config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,14 @@
 Bulldogsvolleyballcom::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => 25,
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 
   # Code is not reloaded between requests
   config.cache_classes = true
